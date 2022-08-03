dccd67c4c30d0e08d6f5a1fdfce1719f8f684647
nicolecht <nicolecht@hotmail.com>
2022-08-03 23:15:24 +0800

    update

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,51 @@
-#include "get_next_line.h"
+//#include "get_next_line.h"
 
-ssize_t read(int fd, void *buf, size_t count);
+//ssize_t read(int fd, void *buf, size_t count);
+
+char    *ft_readandsave(int fd, char *save)
+{
+    char    *buffer;
+    int     *bytes;
+
+    buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!buffer)
+        return (0);
+    bytes = 1;
+
+}
 
 char	*get_next_line(int fd)
 {
-    
+    char    *line;
+    char    *save;
 
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    save = ft_readandsave(fd ,save);
+    if (!save)
+        return (0);
+    return (line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    char    *line;
+    int     i;
+    int     fd1;
+    fd1 = open("test1.txt", O_RDONLY);
+    printf("fd = %d\n", fd1);
+    i = 1;
+    while (i < 3)
+    {
+        line = get_next_line(fd1);
+        printf("Line %d : %s", i, line);
+        free(line);
+        i++;
+    }
+    close(fd1);
+    return (0);
 }
