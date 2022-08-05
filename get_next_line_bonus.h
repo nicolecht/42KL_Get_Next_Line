9204757dc3c53518d00e150234eeb881b620ef35
nicolecht <nicolecht@hotmail.com>
2022-08-05 17:59:07 +0800

    completed

---
+++ get_next_line_bonus.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char    *read_line(int fd, char *str);
+char    *get_line(char *str);
+char    *update_line(char *str);
+char    *ft_strchr(char *str, int c);
+char    *ft_strjoin(char *s1, char *s2);
+size_t  ft_strlen(char *str);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,62 @@
+#include "get_next_line_bonus.h"
+
+size_t ft_strlen(char *str)
+{
+    size_t i;
+    
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
+char    *ft_strchr(char *str, int c)
+{
+    int i;
+    
+    i = 0;
+    if (!str)
+        return (0);
+    while (str[i])
+    {
+        if (str[i] == (char)c)
+            return ((char *)(str + i));
+        i++;
+    }
+    if (str[i] == (char)c)
+        return ((char *)(str + i));
+    return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+    if (!s1)
+    {
+        s1 = (char *)malloc(1 * sizeof(char));
+        s1[0] = '\0';
+    }
+	if (!s1 || !s2)
+		return (0);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+    free(s1);
+    return (dest);
+}
